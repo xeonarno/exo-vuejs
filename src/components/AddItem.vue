d8eaab3751790c813ca66f3a29a69e6258e32ffe
Arnaud Palin Sainte Agathe <xeonarno@hotmail.com>
2023-10-10 14:27:06 +0200

    [EXO] feat: ✨ add Button to Add App

---
+++ src/components/AddItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-card class="application-thumbnail" align-center>
+        <v-col>        
+            <v-btn class="ma-4" color="primary" @click="openAppDialog">
+                <v-icon left>mdi-plus</v-icon>
+            </v-btn>
+        </v-col>
+    </v-card>
+</template>
+
+<script lang="ts">
+export default {
+    methods: {
+        openAppDialog() {
+            console.log('AddItem > openDialog');
+            this.$emit("add-application", true);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.application-thumbnail {
+    cursor: pointer;
+    width: 200px;
+    margin: 10px;
+}
+</style>
